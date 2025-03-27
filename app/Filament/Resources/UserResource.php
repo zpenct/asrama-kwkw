a06b462cd60f3f24865714d2bdf16d6631ed7631
Fayzul <fayzulhaq0708@gmail.com>
2025-03-27 16:47:35 +0800

    fix: fix issue edit and redirect user

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -3,42 +3,51 @@
 namespace App\Filament\Resources;
 
 use App\Filament\Resources\UserResource\Pages;
-use App\Filament\Resources\UserResource\RelationManagers;
 use App\Models\User;
 use Filament\Forms;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
 use Filament\Tables\Table;
-use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\SoftDeletingScope;
 
 class UserResource extends Resource
 {
     protected static ?string $model = User::class;
 
     protected static ?string $navigationIcon = 'heroicon-o-user-group';
+
     protected static ?string $navigationGroup = 'Manajemen User';
+
     protected static ?string $label = 'Pengguna';
 
     public static function form(Form $form): Form
     {
+
         return $form
             ->schema([
                 Forms\Components\TextInput::make('name')
                     ->label('Nama user')
+                    ->disabledOn('edit')
                     ->required(),
+
                 Forms\Components\TextInput::make('email')
                     ->label('Masukkan Email')
+                    ->disabledOn('edit')
+                    ->unique(User::class, 'email', ignoreRecord: true)
                     ->required(),
+
                 Forms\Components\Select::make('roles')
                     ->label('Masukkan Role')
                     ->relationship('roles', 'name')
                     ->required(),
+
                 Forms\Components\TextInput::make('password')
                     ->label('Password')
-                    ->required()
-                    ->placeholder('Masukkan Password'),
+                    ->placeholder('Masukkan Password')
+                    ->disabledOn('edit')
+                    ->password()
+                    ->dehydrated(fn ($state) => filled($state))
+                    ->required(fn () => request()->routeIs('filament.resources.users.create')),
             ]);
     }
 
@@ -49,6 +58,7 @@ class UserResource extends Resource
                 Tables\Columns\TextColumn::make('name')->label('Nama user')->searchable(),
                 Tables\Columns\TextColumn::make('roles.name')->label('Role user')->searchable(),
                 Tables\Columns\TextColumn::make('email')->label('Email')->searchable(),
+                Tables\Columns\CheckboxColumn::make('is_first')->label('Reset Password')->disabled(),
             ])
             ->filters([
                 //
--- app/Filament/Resources/UserResource/Pages/EditUser.php
+++ app/Filament/Resources/UserResource/Pages/EditUser.php
@@ -5,6 +5,7 @@ namespace App\Filament\Resources\UserResource\Pages;
 use App\Filament\Resources\UserResource;
 use Filament\Actions;
 use Filament\Resources\Pages\EditRecord;
+use Illuminate\Support\Facades\Hash;
 
 class EditUser extends EditRecord
 {
@@ -15,5 +16,21 @@ class EditUser extends EditRecord
         return [
             Actions\DeleteAction::make(),
         ];
+    }
+
+    protected function mutateFormDataBeforeSave(array $data): array
+    {
+        if (! empty($data['password'])) {
+            $data['password'] = Hash::make($data['password']);
+        } else {
+            unset($data['password']);
+        }
+
+        return $data;
+    }
+
+    protected function getRedirectUrl(): string
+    {
+        return UserResource::getUrl('index');
     }
 }
