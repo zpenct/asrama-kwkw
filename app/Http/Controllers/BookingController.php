7d1925d5abf016ba0640fb28bbffacca5ba3e6a0
Seven4T <kamalmt22d@student.unhas.ac.id>
2025-05-22 20:50:32 +0800

    fix navbar dan urutan transactions di booking page

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -52,7 +52,7 @@ class BookingController extends Controller
 
     public function showTransactions()
     {
-        $booking = Booking::with('room')->where('user_id', Auth::id())->get();
+        $booking = Booking::with('room')->where('user_id', Auth::id())->orderBy('created_at', 'desc')->get();
 
         return view('transactions.page', compact('booking'));
     }
