b13608c0e4c69166c7bb15003e5c17ced15d6237
Fayzul <fayzulhaq0708@gmail.com>
2025-05-23 01:21:33 +0800

    fix: fix assign role when sync user

--- app/Jobs/SyncUsersJob.php
+++ app/Jobs/SyncUsersJob.php
@@ -6,16 +6,12 @@ use App\Models\User;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Queue\Queueable;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Log;
 use Spatie\Permission\Models\Role;
 
 class SyncUsersJob implements ShouldQueue
 {
     use Queueable;
 
-    /**
-     * Create a new job instance.
-     */
     public function __construct()
     {
         //
@@ -40,11 +36,12 @@ class SyncUsersJob implements ShouldQueue
 
             // Assign Role using Spatie
             $role = Role::firstOrCreate(['name' => 'user']);
-            if (! $user->hasRole('user')) {
-                $user->assignRole($role);
-                Log::channel('sync')->info("Assigned role 'user' to user {$user->email}");
-                Log::info("Assigned role 'user' to user {$user->email}");
-            }
+            $user->assignRole($role);
+
+            logger()->info('CHECK For:', [
+                'student' => $user,
+            ]);
+
         }
     }
 }
