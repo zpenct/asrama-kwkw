1380e0a10008ef62ba79beaf0ae50c12ba6bf2a7
Fayzul <fayzulhaq0708@gmail.com>
2025-05-23 11:46:12 +0800

    chore: trigger new ci

--- app/Policies/UserPolicy.php
+++ app/Policies/UserPolicy.php
@@ -3,19 +3,12 @@
 namespace App\Policies;
 
 use App\Models\User;
-
 use Illuminate\Auth\Access\HandlesAuthorization;
 
 class UserPolicy
 {
     use HandlesAuthorization;
 
-    /**
-     * Determine whether the user can view any models.
-     *
-     * @param  \App\Models\User  $user
-     * @return bool
-     */
     public function viewAny(User $user): bool
     {
         return $user->can('view_any_user');
@@ -23,9 +16,6 @@ class UserPolicy
 
     /**
      * Determine whether the user can view the model.
-     *
-     * @param  \App\Models\User  $user
-     * @return bool
      */
     public function view(User $user): bool
     {
@@ -34,9 +24,6 @@ class UserPolicy
 
     /**
      * Determine whether the user can create models.
-     *
-     * @param  \App\Models\User  $user
-     * @return bool
      */
     public function create(User $user): bool
     {
@@ -45,9 +32,6 @@ class UserPolicy
 
     /**
      * Determine whether the user can update the model.
-     *
-     * @param  \App\Models\User  $user
-     * @return bool
      */
     public function update(User $user): bool
     {
@@ -56,9 +40,6 @@ class UserPolicy
 
     /**
      * Determine whether the user can delete the model.
-     *
-     * @param  \App\Models\User  $user
-     * @return bool
      */
     public function delete(User $user): bool
     {
@@ -67,9 +48,6 @@ class UserPolicy
 
     /**
      * Determine whether the user can bulk delete.
-     *
-     * @param  \App\Models\User  $user
-     * @return bool
      */
     public function deleteAny(User $user): bool
     {
@@ -78,9 +56,6 @@ class UserPolicy
 
     /**
      * Determine whether the user can permanently delete.
-     *
-     * @param  \App\Models\User  $user
-     * @return bool
      */
     public function forceDelete(User $user): bool
     {
@@ -89,9 +64,6 @@ class UserPolicy
 
     /**
      * Determine whether the user can permanently bulk delete.
-     *
-     * @param  \App\Models\User  $user
-     * @return bool
      */
     public function forceDeleteAny(User $user): bool
     {
@@ -100,9 +72,6 @@ class UserPolicy
 
     /**
      * Determine whether the user can restore.
-     *
-     * @param  \App\Models\User  $user
-     * @return bool
      */
     public function restore(User $user): bool
     {
@@ -111,9 +80,6 @@ class UserPolicy
 
     /**
      * Determine whether the user can bulk restore.
-     *
-     * @param  \App\Models\User  $user
-     * @return bool
      */
     public function restoreAny(User $user): bool
     {
@@ -122,9 +88,6 @@ class UserPolicy
 
     /**
      * Determine whether the user can bulk restore.
-     *
-     * @param  \App\Models\User  $user
-     * @return bool
      */
     public function replicate(User $user): bool
     {
@@ -133,9 +96,6 @@ class UserPolicy
 
     /**
      * Determine whether the user can reorder.
-     *
-     * @param  \App\Models\User  $user
-     * @return bool
      */
     public function reorder(User $user): bool
     {
