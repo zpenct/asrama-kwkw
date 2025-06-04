c9e81933599d793a9d437b4bdc4c4bad243ff54f
Fayzul <fayzulhaq0708@gmail.com>
2025-06-04 09:38:48 +0800

    fix: fix dashboard

--- resources/views/filament/pages/dashboard.blade.php
+++ resources/views/filament/pages/dashboard.blade.php
@@ -1,7 +1,15 @@
 <x-filament-panels::page>
-    <div class="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
+    <div class="grid grid-cols-1 gap-4">
         @foreach ($this->getHeaderWidgets() as $widget)
-            {{ $widget }}
+            @if ($widget::class === \App\Filament\Widgets\AnnualRevenueChart::class)
+                <div class="col-span-1 sm:col-span-1 lg:col-span-3">
+                    {{ $widget }}
+                </div>
+            @else
+                <div class="col-span-1 sm:col-span-1 lg:col-span-1">
+                    {{ $widget }}
+                </div>
+            @endif
         @endforeach
     </div>
 </x-filament-panels::page>
